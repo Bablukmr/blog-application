5cff5945c108b027c21c8728df7fef5ba5a6bfc4
Bablukmr <bablukmr800@gmail.com>
2023-08-03 23:24:23 +0530

    contact page completed

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
 import styles from './page.module.css'
+import Image from 'next/image';
+import IMG from 'public/contact.png'
+import Button from '@/components/Button/Button';
 
 export default function Contact() {
   return (
-    <div className={styles.contact}>Contact</div>
+    <div className={styles.contact}>
+      <h1 className={styles.title}>Let's keep in Touch</h1>
+      <div className={styles.maincontainer}>
+      <div className={styles.imgcontainer}>
+        <Image src={IMG} alt='IMG' fill={true} className={styles.images}/>
+      </div>
+<form className={styles.form}>
+<input placeholder='Name' className={styles.input}/>
+<input placeholder='Email' className={styles.input}/>
+<textarea placeholder='Message' cols="30" rows="10" className={styles.textArea}/>
+<Button url={"#"} text={"Send"}/>
+</form>
+    </div>
+    </div>
   )
 }
