14f4a4b17e9fec08adf1cfaa32a7c3811ac96b42
Bablukmr <bablukmr800@gmail.com>
2023-08-06 16:19:05 +0530

    correction

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './page.module.css';
 import Image from 'next/image';
-import IMG from '/public/contact.png'; // Corrected import path
+import IMG from '/public/contact.png';
 import Button from '@/components/Button/Button';
 
 export const metadata = {
@@ -12,16 +12,16 @@ export const metadata = {
 export default function Contact() {
   return (
     <div className={styles.contact}>
-      <h1 className={styles.title}>Let's keep in Touch</h1>
+      <h1 className={styles.title}>Let&apos;s keep in Touch</h1> {/* Replaced ' with &apos; */}
       <div className={styles.maincontainer}>
         <div className={styles.imgcontainer}>
-          <Image src={IMG} alt='IMG' layout="fill" className={styles.images} /> {/* Used layout="fill" instead of fill={true} */}
+          <Image src={IMG} alt='IMG' layout="fill" className={styles.images} />
         </div>
         <form className={styles.form}>
           <input placeholder='Name' className={styles.input} />
           <input placeholder='Email' className={styles.input} />
           <textarea placeholder='Message' cols="30" rows="10" className={styles.textArea} />
-          <Button url={"#"} text={"Send"} /> {/* Make sure the Button component is receiving the correct props */}
+          <Button url={"#"} text={"Send"} />
         </form>
       </div>
     </div>
