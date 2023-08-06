637fe73ff6fd16519813cb6940fbe072717e47b5
Bablukmr <bablukmr800@gmail.com>
2023-08-06 16:55:16 +0530

    process.env.NEXTAUTH_URL

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 //process.env.NEXTAUTH_URL
 async function getData(){
-      const res= await fetch("process.env.NEXTAUTH_URL/api/posts", {
+      const res= await fetch(process.env.NEXTAUTH_URL/"api/posts", {
             cache: "no-store",
           });
       if(!res.ok){
