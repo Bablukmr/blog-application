f2e39a73684668de600100caf4d5dbceddeef442
Bablukmr <bablukmr800@gmail.com>
2023-08-06 19:16:34 +0530

    getData

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
     images: {
         domains: ['plus.unsplash.com','images.unsplash.com','images.pexels.com',"drive.google.com"],
       },
+      eslint:{
+        ignoreDuringBuilds:true
+      },
 }
 
 module.exports = nextConfig
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -3,15 +3,31 @@ import styles from './blog.module.css'
 import Image from 'next/image'
 import Link from 'next/link'
 //process.env.NEXTAUTH_URL
-async function getData(){
-      const res= await fetch(`${process.env.NEXTAUTH_URL}/api/posts`, {
-            cache: "no-store",
-          });
-      if(!res.ok){
-            console.log("error");
+// async function getData(){
+//       const res= await fetch(`${process.env.NEXTAUTH_URL}/api/posts`, {
+//             cache: "no-store",
+//           });
+//       if(!res.ok){
+//             console.log("error");
+//       }
+//       return res.json()
+// }
+async function getData() {
+      try {
+        const res = await fetch(`${process.env.NEXTAUTH_URL}/api/posts`, {
+          cache: "no-store",
+        });
+    
+        if (!res.ok) {
+          throw new Error("Failed to fetch data");
+        }
+    
+        return res.json();
+      } catch (error) {
+        console.error("Error fetching data:", error.message);
+        return []; // Return an empty array to handle the error gracefully
       }
-      return res.json()
-}
+    }
 
 export default async function Bolg({ params }) {
       const data=await getData()
