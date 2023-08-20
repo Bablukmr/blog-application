2bc3e5be3d147cf70170e52d2cc1ce50ac722f39
Bablukmr <bablukmr800@gmail.com>
2023-08-20 18:38:43 +0530

    mobile responsive

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -14,48 +14,48 @@ import Link from 'next/link'
 // }
 async function getData() {
       try {
-        const res = await fetch(`${process.env.NEXTAUTH_URL}/api/posts`, {
-          cache: "no-store",
-        });
-    
-        if (!res.ok) {
-          throw new Error("Failed to fetch data");
-        }
-    
-        return res.json();
+            const res = await fetch(`${process.env.NEXTAUTH_URL}/api/posts`, {
+                  cache: "no-store",
+            });
+
+            if (!res.ok) {
+                  throw new Error("Failed to fetch data");
+            }
+
+            return res.json();
       } catch (error) {
-        console.error("Error fetching data:", error.message);
-        return []; // Return an empty array to handle the error gracefully
+            console.error("Error fetching data:", error.message);
+            return []; // Return an empty array to handle the error gracefully
       }
-    }
-    const data=await getData()
-    console.log(data[0].content)
+}
+const data = await getData()
+console.log(data[0].content)
 export default async function Bolg({ params }) {
-      const data=await getData()
+      const data = await getData()
       return (
             <div className={styles.maincontainer}>
                   {
-                        data.map((item)=>(
-                   
-                  <Link href={`/blog/${item._id}`} className={styles.container} key={item.id}>
-                        <div className={styles.imgContainer}>
-                              <Image src={item.img}
-                                    alt=''
-                                    width={400}
-                                    height={250} className={styles.img} />
-                        </div>
-                        <div className={styles.content}>
-                              <h1 className={styles.title}>
-                                    {item.title}
-                              </h1>
-                              <p className={styles.desc}>
-                                    {item.desc}
-                              </p>
-                        </div>
-                  </Link>
-                       ))
+                        data.map((item) => (
+
+                              <Link href={`/blog/${item._id}`} className={styles.container} key={item.id}>
+                                    <div className={styles.imgContainer}>
+                                          <Image src={item.img}
+                                                alt=''
+                                                width={400}
+                                                height={250} className={styles.img} />
+                                    </div>
+                                    <div className={styles.content}>
+                                          <h1 className={styles.title}>
+                                                {item.title}
+                                          </h1>
+                                          <p className={styles.desc}>
+                                                {item.desc}
+                                          </p>
+                                    </div>
+                              </Link>
+                        ))
                   }
-                 
+
             </div>
       )
 }
@@ -63,4 +63,4 @@ export default async function Bolg({ params }) {
 export const metadata = {
       title: data[0].content,
       description: 'Blog Application create through next.js app',
-    }
+}
